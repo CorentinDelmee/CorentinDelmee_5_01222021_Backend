1596e6728e4377da21441c8243ea19447258e73e
CorentinDelmee <corentinmdelmee@gmail.com>
2022-02-01 18:35:43 +0100

    Ajout des fichiers back de base

---
+++ app.js
@@ -0,0 +1,36 @@
+// Appel du module Express
+
+const express = require('express');
+
+// Appel du module mongoose
+
+const mongoose = require("mongoose");
+
+// Connexion mongoose Database
+
+mongoose.connect('mongodb+srv://DefaultUser:MS64dLr6WqetV2a7@cluster0.bdfdk.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Initialisation de l'app
+
+const app = express();
+app.use(express.json());
+
+// Header CORS
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+// Utilisation des routes
+
+
+// Export de l'app pour server.js
+
+module.exports = app;
