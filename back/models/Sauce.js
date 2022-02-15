dc32995aab88546b2a34a2f739afa9dbfeb35d3f
CorentinDelmee <corentinmdelmee@gmail.com>
2022-02-15 15:28:25 +0100

    Required model sauce

--- back/models/Sauce.js
+++ back/models/Sauce.js
@@ -5,17 +5,17 @@ const mongoose = require("mongoose");
 // Création d'un schema d'objet
 
 const saucesSchema = mongoose.Schema({
-    userId: { type: String },
-    name: { type: String },
-    manufacturer: { type: String },
-    description: { type: String },
-    mainPepper: { type: String },
-    imageUrl: { type: String },
-    heat: { type: Number },
-    likes: { type: Number },
-    dislikes: { type: Number },
-    usersLiked: { type: [String] },
-    usersDisliked: { type: [String] },
+    userId: { type: String , required: true},
+    name: { type: String, required: true},
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    usersLiked: { type: [String], required: true },
+    usersDisliked: { type: [String], required: true },
 
 })
 
